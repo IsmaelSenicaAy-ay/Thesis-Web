74b8906ce36dd825785c5a1693ad2a369b6104d0
venciapps3321 <ismaelsenica@gmail.com>
2024-02-02 14:07:58 +0800

    yes

---
+++ Thesis-Website/THESIS/MAIN/pressure.js
@@ -0,0 +1,12 @@
+let leftFoot = document.getElementById('left-foot');
+let rightFoot = document.getElementById('right-foot');
+
+let leftFootHeel = document.getElementById('left-foot-heel');
+let leftFootToes = document.getElementById('left-foot-toes');
+let rightFootHeel = document.getElementById('right-foot-heel');
+let rightFootToes = document.getElementById('right-foot-toes');
+
+leftFootHeel.style.borderRadius = '50% 50% 0% 0%';
+leftFootToes.style.borderRadius = '0% 0% 50% 50%';
+rightFootHeel.style.borderRadius = '50% 50% 0% 0%';
+rightFootToes.style.borderRadius = '0% 0% 50% 50%';
